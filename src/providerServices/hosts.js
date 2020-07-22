a5cbe1fc41b64dbb56ddc842da3ee88417209b60
Cristian Andres Marin Durango <camd1996@gmail.com>
2020-07-22 15:07:19 -0500

    last refactors

--- src/providerServices/hosts.js
+++ src/providerServices/hosts.js
@@ -1,9 +1,9 @@
 module.exports = {
     CIRENE: {
-        HOST: 'http://cirene.udea.edu.co'
+        HOST: 'https://cirene.udea.edu.co'
     },
     TURNOS: {
-        HOST: /*'http://localhost/turnos/services'*/ 'http://biblioteca.udea.edu.co/turnos/services'
+        HOST: /*'http://localhost/turnos/services'*/ 'https://biblioteca.udea.edu.co/turnos/services'
     },
     LIBRARIES: {
         HOST: 'http://4c491fb9.ngrok.io/api'
